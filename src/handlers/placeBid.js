8448bc0321ebe302d54906ec3fc911d240b1c40d
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-25 21:56:24 -0500

    get Auction by id function reusable

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk');
+const createError = require('http-errors');
+const commonMiddleware = require('../lib/commonMiddleware');
+const { getAuctionById } = require('./getAuction');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const auction = await getAuctionById(id);
+
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid mus be higher that ${auction.highestBid.amount}`
+    );
+  }
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+module.exports = {
+  handler: commonMiddleware(placeBid),
+};
