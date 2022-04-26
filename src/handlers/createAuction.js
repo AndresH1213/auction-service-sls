1be8ce79cba2b08e52a29cb4a0ef58cdab74e4fc
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-25 21:57:14 -0500

    Auctions crud, placebid and middlewares

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,13 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
+const middy = require('@middy/core');
+const createError = require('http-errors');
+const commonMiddleware = require('../lib/commonMiddleware');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+async function createAuction(event, context) {
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -12,17 +15,28 @@ exports.handler = async function createAuction(event, context) {
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    },
   };
-
-  await dynamodb
-    .put({
-      TableName: 'AuctionsTable',
-      Item: auction,
-    })
-    .promise();
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
   };
+}
+
+module.exports = {
+  handler: commonMiddleware(createAuction),
 };
---
+++ src/handlers/getAuctions.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const commonMiddleware = require('../lib/commonMiddleware');
+const createError = require('http-errors');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  try {
+    const result = await dynamodb
+      .scan({ TableName: process.env.AUCTIONS_TABLE_NAME })
+      .promise();
+
+    auctions = result.Items;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+}
+
+module.exports = {
+  handler: commonMiddleware(getAuctions),
+};
